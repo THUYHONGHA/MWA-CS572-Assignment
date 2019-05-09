849f370ab144ed283f6b8c9ac30a6d1fae26fea9
THUYHONGHA <thuyhong.ip@gmail.com>
2019-05-09 06:16:56 -0500

    Lab7_Exercise3_Complete

--- Lab7/Lab7_Exercise3/routes/index.js
+++ Lab7/Lab7_Exercise3/routes/index.js
@@ -3,30 +3,48 @@ var router = express.Router();
 var crud = require('../service/crud');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/lectures', function(req, res, next) {
-  crud.Find().then((docs)=> {
-    const data = JSON.stringify(docs);
-    res.send(JSON.parse(data));
-  }).catch(err=> console.log("error at index"+(err)));
+router.get('/lectures', function (req, res, next) {
+    crud.Find().then((docs) => {
+        const data = JSON.stringify(docs);
+        res.send(JSON.parse(data));
+    }).catch(err => console.log("error at index" + (err)));
 });
 
-router.get('/lectures/:course', function(req, res, next) {
-  crud.FindOne(req.params.course).then((doc)=> {
-    const data = JSON.stringify(doc);
-    res.send(JSON.parse(data));
-  }).catch(err=> console.log("error at index"+JSON.stringify(err)));
+router.get('/lectures/:course', function (req, res, next) {
+    crud.FindOne(req.params.course).then((doc) => {
+        const data = JSON.stringify(doc);
+        res.send(JSON.parse(data));
+    }).catch(err => console.log("error at index" + JSON.stringify(err)));
 });
 
-router.post('/lectures', function(req, res, next) {
-  console.log("start index");
-  crud.FindOne(req.params.course).then((doc)=> {
-    const data = JSON.stringify(doc);
-    res.send(JSON.parse(data));
-  }).catch(err=> console.log("error at index"+JSON.stringify(err)));
+router.post('/lectures', function (req, res, next) {
+    crud.Add(req.body.course, req.body.lecture)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(400).send(err));
+});
+
+router.put('/lectures/:course', function (req, res, next) {
+    console.log("course" + req.params.course);
+    crud.Update(req.params.course, req.body.lecture)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(400).send(err));
+});
+
+router.delete('/lectures/:course', function (req, res, next) {
+    console.log("course" + req.params.course);
+    crud.Delete(req.params.course)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(400).send(err));
+});
+
+router.get('/lectures/search/:q', function (req, res, next) {
+    crud.Search(req.params.q)
+        .then(data => res.status(200).send(JSON.parse(data)))
+        .catch(err => res.status(400).send(err));
 });
 module.exports = router;
 
--- Lab7/Lab7_Exercise3/service/crud.js
+++ Lab7/Lab7_Exercise3/service/crud.js
@@ -1,16 +1,16 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://homework07:homework07@cluster0-fleow.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
 
 
 function Find() {
-    return new Promise((resolve, reject) =>{
+    return new Promise((resolve, reject) => {
+        let client = new MongoClient(uri, {useNewUrlParser: true});
         client.connect(err => {
             const collection = client.db("homework07").collection("homework07");
-            collection.find({}, {'_id': 0}).toArray((err, docs) => {
+            collection.find().project({_id: 0}).toArray((err, docs) => {
                 if (err) reject(err);
-                else{
-                    console.log("crud file"+ docs);
+                else {
+                    console.log("crud file" + docs);
                     resolve(docs);
                     console.log("before close");
                     client.close();
@@ -22,66 +22,110 @@ function Find() {
 }
 
 function FindOne(course) {
-    return new Promise((resolve, reject) =>{
+    return new Promise((resolve, reject) => {
+        let client = new MongoClient(uri, {useNewUrlParser: true});
         client.connect(err => {
             const collection = client.db("homework07").collection("homework07");
-            collection.findOne({'course': course}, {'_id': 0}).then(doc => resolve(doc));
+            collection.findOne({'course': course}, {projection: {_id: 0}}, (err, doc) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(doc);
+            });
             client.close();
         });
     });
 }
 
 function Add(course, lecture) {
-    return new Promise((resolve, reject) =>{
+    return new Promise((resolve, reject) => {
+        let client = new MongoClient(uri, {useNewUrlParser: true});
         client.connect(err => {
             const collection = client.db("homework07").collection("homework07");
-            let isCourseExist = collection.findOne({'course': course}, {'_id': 0});
-            if(isCourseExist.count>0){
-                return
-            }
-            client.close();
+            isIdExist = collection.findOne({'course': course}, (err, doc) => {
+                console.log("error" + err);
+                console.log("Add doc: " + doc);
+                if (err) {
+                    reject(err);
+                    client.close();
+                } else if (doc) {
+                    reject({"message": "This course is existing."});
+                    client.close();
+                } else
+                    collection.insert({'course': course, 'lecture': lecture}, (err, result) => {
+                        console.log("error with new course" + err);
+                        console.log("succedd with new course" + JSON.stringify(result));
+                        if (err) reject(err)
+                        else resolve({"message": "Inserting succeed!"});
+                        client.close();
+                    });
+            });
+
         });
     });
-    if (course) {
-        let isCourseExist = data.filter(item => item.id == id);
-        if (isCourseExist.length > 0)
-            return {status: 400, message: 'Id is existing!'};
-        else {
-            data.push(id, name, course, grade);
-            return {status: 200, message: 'Successful creating!'};
-        }
-    } else {
-        return {status: 400, message: 'ID is empty!'};
+}
+
+function Update(course, lecture) {
+    return new Promise((resolve, reject) => {
+        let client = new MongoClient(uri, {useNewUrlParser: true});
+        client.connect(err => {
+            const collection = client.db("homework07").collection("homework07");
+            collection.updateOne({"course": course}, {$set: {"lecture": lecture}}, (err, result) => {
+                console.log("err: " + JSON.stringify(err));
+                console.log("result " + JSON.stringify(result));
+                if (err) reject({"message": "This course is not existing."})
+                else {
+
+                    resolve({"message": "Updating succeed!"});
+                }
+                client.close();
+            });
+        });
+    });
+}
+
+function Delete(course) {
+    return new Promise((resolve, reject) => {
+        let client = new MongoClient(uri, {useNewUrlParser: true});
+        client.connect(err => {
+            const collection = client.db("homework07").collection("homework07");
+            collection.deleteOne({'course': course}, (err, result) => {
+                if (err) reject({"message": "Deleting failed!"});
+                else if(result.n>0){
+                    resolve({"message": "Deleting succeed"});
+                }else {
+                    resolve({"message": "This item is not existing"});
+                }
+                client.close();
+            });
+        });
+    });
+}
+
+function Search(content){
+    return new Promise((resolve, reject) => {
+        let client = new MongoClient(uri, {useNewUrlParser: true});
+        client.connect(err => {
+            console.log("content: "+ content);
+            const collection = client.db("homework07").collection("homework07");
+            collection.find({$or: [{'course': {$regex: '.*'+ content +'.*', $options: 'i'}},
+                    {'lecture': {$regex: '.*'+ content +'.*', $options: 'i'}}]}).toArray((err, result)=>{
+                console.log("error "+ JSON.stringify(err));
+                console.log("succeed: "+ JSON.stringify(result));
+                if (err) reject(JSON.stringify(err))
+                resolve(JSON.stringify(result));
+                client.close();
+            });
+        });
+    });
+}
+
+    module.exports = {
+        Find,
+        FindOne,
+        Add,
+        Update,
+        Delete,
+        Search
     }
-}
-
-function Update(id, grade) {
-    let isIdExist = data.filter(item => item.id == id);
-    if (isIdExist.length > 0) {
-        data.filter(item => item.id == id).map(item => item.grade = grade);
-        return {status: 200, message: 'Successful updating!'};
-    } else {
-        return {status: 400, message: 'This ID is not existing, please create a new one!'};
-    }
-
-}
-
-function Delete(id) {
-    let isIdExist = data.filter(item => item.id == id);
-    if (isIdExist.length > 0) {
-        data = data.filter(item => item.id != id);
-        return {status: 200, message: 'Successful deleting!'};
-    } else {
-        return {status: 400, message: 'This ID is not existing, you cannot delete it!'};
-    }
-
-}
-
-module.exports = {
-    Find,
-    FindOne,
-    Add,
-    Update,
-    Delete
-}
 
